75a8df94f7425926c6c631c9295b7da8c5c485e7
zexuz <robin.edbom@gmail.com>
2020-09-02 21:13:41 +0200

    Add todo

--- Padel.Login/IKeyLoader.cs
+++ Padel.Login/IKeyLoader.cs
@@ -5,6 +5,7 @@ namespace Padel.Login
 {
     public interface IKeyLoader
     {
+        // TODO, it is only in this application (Login) that we want the private key, in all other application, we might only want the public to (In order to validate the token)
         Task<(RSA PublicKey, RSA PrivateKey)> Load();
     }
 }
