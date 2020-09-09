3ad3800e0c644f8214fa547902afd5fa34f3d722
zexuz <robin.edbom@gmail.com>
2020-09-09 19:38:23 +0200

    Add user_repository Me method

--- flutter_app/pad_pal/packages/user_repository/lib/src/user_repository.dart
+++ flutter_app/pad_pal/packages/user_repository/lib/src/user_repository.dart
@@ -1,1 +1,27 @@
-class UserRepository {}
+import 'package:grpc/grpc.dart';
+import 'package:user_repository/generated/user_service.pbgrpc.dart';
+
+class Me {
+  String username;
+  String email;
+  String firstName;
+  String lastName;
+}
+
+class UserRepository {
+  const UserRepository({UserServiceClient userServiceClient}) : _userServiceClient = userServiceClient;
+
+  final UserServiceClient _userServiceClient;
+
+  Future<Me> me(String accessToken) async {
+    final callOptions = CallOptions(metadata: {'Authentication': "Bearer '$accessToken'"});
+    final call = _userServiceClient.me(MeRequest(), options: callOptions);
+
+    final protoRes = await call;
+    return Me()
+      ..email = protoRes.me.email
+      ..username = protoRes.me.username
+      ..firstName = protoRes.me.firstName
+      ..lastName = protoRes.me.lastName;
+  }
+}
---
+++ flutter_app/pad_pal/packages/user_repository/test/user_repository_test.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:grpc/grpc.dart';
+import 'package:mockito/mockito.dart';
+import 'package:user_repository/generated/user_service.pbgrpc.dart';
+import 'package:user_repository/generated/user_service.pbgrpc.dart' as pb;
+import 'package:user_repository/src/user_repository.dart';
+
+class MockUserServiceClient extends Mock implements UserServiceClient {}
+
+class MockResponseFuture<T> extends Mock implements ResponseFuture<T> {
+  final T value;
+
+  MockResponseFuture(this.value);
+
+  Future<S> then<S>(FutureOr<S> onValue(T value), {Function onError}) =>
+      Future.value(value).then(onValue, onError: onError);
+}
+
+void main() {
+  const accessToken = 'myAccessToken';
+
+  group('UserRepository', () {
+    UserRepository userRepository;
+    UserServiceClient userServiceClient;
+
+    setUp(() {
+      userServiceClient = MockUserServiceClient();
+
+      userRepository = UserRepository(userServiceClient: userServiceClient);
+    });
+
+    group('Me', () {
+      test('should get my profile info', () async {
+        when(userServiceClient.me(any, options: anyNamed('options'))).thenAnswer(
+              (_) =>
+              MockResponseFuture<MeResponse>(MeResponse()
+                ..me = (pb.Me()
+                  ..username = "zexuz"
+                  ..email = "robin@mkdir.se"
+                  ..firstName = "robin"
+                  ..lastName = "edbom")),
+        );
+
+        var res = await userRepository.me(accessToken);
+
+        expect(res.username, "zexuz");
+        expect(res.email, "robin@mkdir.se");
+        expect(res.firstName, "robin");
+        expect(res.lastName, "edbom");
+
+        var verification = verify(userServiceClient.me(any, options: captureAnyNamed('options')));
+        expect((verification.captured[0] as CallOptions).metadata, {'Authentication': 'Bearer \'myAccessToken\''});
+      });
+    });
+  });
+}
