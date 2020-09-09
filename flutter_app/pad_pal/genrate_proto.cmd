99d34ad484863515659b44dfe8af7b5338c0a360
zexuz <robin.edbom@gmail.com>
2020-09-09 18:22:53 +0200

    Add user_repository package

--- flutter_app/pad_pal/genrate_proto.cmd
+++ flutter_app/pad_pal/genrate_proto.cmd
@@ -1,1 +1,2 @@
-protoc -I ./../../Padel.Runner/Protos ./../../Padel.Runner/Protos/auth_service.proto --plugin=protoc-gen-dart=c:\Users\desktop\AppData\Roaming\Pub\Cache\bin\protoc-gen-dart.bat --dart_out=grpc:lib/generated
+protoc -I ./../../Padel.Runner/Protos ./../../Padel.Runner/Protos/auth_service.proto --plugin=protoc-gen-dart=c:\Users\desktop\AppData\Roaming\Pub\Cache\bin\protoc-gen-dart.bat --dart_out=grpc:packages/authentication_repository/lib/generated
+protoc -I ./../../Padel.Runner/Protos ./../../Padel.Runner/Protos/user_service.proto --plugin=protoc-gen-dart=c:\Users\desktop\AppData\Roaming\Pub\Cache\bin\protoc-gen-dart.bat --dart_out=grpc:packages/user_repository/lib/generated
