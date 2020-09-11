53b705b9ba617032528d3b56c83e9abc85996c7b
robin edbom <robin.edbom@gmail.com>
2020-09-11 15:05:01 +0200

    Create path to component list

---
+++ flutter_app/pad_pal/lib/components/view.dart
@@ -0,0 +1,1 @@
+export 'view/components_page.dart';
--- flutter_app/pad_pal/lib/splash/view/splash_page.dart
+++ flutter_app/pad_pal/lib/splash/view/splash_page.dart
@@ -18,16 +18,3 @@ class SplashPage extends StatelessWidget {
   }
 }
 
-class Login extends StatelessWidget {
-  static Route route() {
-    return MaterialPageRoute<void>(builder: (_) => Login());
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Text("LoginPage"),
-    ));
-  }
-}
