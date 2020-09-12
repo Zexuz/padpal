3c09462e37f96947c3d2ced1a002773348a77f64
robin edbom <robin.edbom@gmail.com>
2020-09-12 11:35:19 +0200

    Fix demo page

--- flutter_app/pad_pal/lib/demo/botton_demo.dart
+++ flutter_app/pad_pal/lib/demo/botton_demo.dart
@@ -26,16 +26,12 @@ class ButtonDemo extends StatelessWidget {
 
     return [
       ButtonLargePrimary(onPressed: onPress, text: text, stretch: true, isDisabled: false),
-      ButtonLargePrimary(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonSmallPrimary(onPressed: onPress, text: text, stretch: true, isDisabled: false),
-      ButtonSmallPrimary(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonLargeSecondary(onPressed: onPress, text: text, stretch: true, isDisabled: false),
-      ButtonLargeSecondary(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonSmallSecondary(onPressed: onPress, text: text, stretch: true, isDisabled: false),
-      ButtonSmallSecondary(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonLargeLight(onPressed: onPress, text: text, stretch: true, isDisabled: false),
-      ButtonLargeLight(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonSmallLight(onPressed: onPress, text: text, stretch: true, isDisabled: false),
+      ButtonLargeLight(onPressed: onPress, text: text, stretch: true, isDisabled: true),
       ButtonSmallLight(onPressed: onPress, text: text, stretch: true, isDisabled: true),
     ];
   }
