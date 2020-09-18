25de693976d9e75ae9360921e7a7f83957fa3028
zexuz <robin.edbom@gmail.com>
2020-09-19 00:10:12 +0200

    Updated genr

--- flutter_app/pad_pal/genrate_proto.sh
+++
@@ -1,5 +0,0 @@
-mkdir packages/user_repository/lib/generated
-protoc -I ./../../Padel.Runner/Protos ./../../Padel.Runner/Protos/user_service.proto --plugin=protoc-gen-dart=/usr/local/bin/protoc-gen-dart --dart_out=grpc:packages/user_repository/lib/generated
-mkdir packages/authentication_repository/lib/generated
-protoc -I ./../../Padel.Runner/Protos ./../../Padel.Runner/Protos/auth_service.proto --plugin=protoc-gen-dart=/usr/local/bin/protoc-gen-dart --dart_out=grpc:packages/authentication_repository/lib/generated
-
