8eb518fd34ceb1ee10f1dcb21058f74723057084
zexuz <robin.edbom@gmail.com>
2020-09-23 19:40:15 +0200

    Add auth gateway

--- gateway/main.go
+++ gateway/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"github.com/mkdir-sweden/padpal/gateway/auth"
+	"github.com/mkdir-sweden/padpal/gateway/authpb"
 	"github.com/mkdir-sweden/padpal/gateway/chat"
 	"github.com/mkdir-sweden/padpal/gateway/chatpb"
 	"google.golang.org/grpc"
@@ -28,11 +30,17 @@ func main() {
 	opts = append(opts, grpc.WithInsecure())
 
 	opts = append(opts, grpc.WithBlock())
-	conn, err := grpc.Dial("localhost:5001", opts...)
+	chatConn, err := grpc.Dial("localhost:5001", opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
-	defer conn.Close()
+
+	authConn, err := grpc.Dial("localhost:5002", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer chatConn.Close()
+	defer authConn.Close()
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -40,7 +48,8 @@ func main() {
 	}
 	s := grpc.NewServer()
 
-	chatpb.RegisterChatServiceService(s, chat.NewChatService(conn))
+	chatpb.RegisterChatServiceService(s, chat.NewChatService(chatConn))
+	authpb.RegisterAuthServiceService(s, auth.NewAuthService(authConn))
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
