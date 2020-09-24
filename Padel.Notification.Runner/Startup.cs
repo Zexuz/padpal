154ece312ba6a1f0dda5bfeb62d465870a4f7afc
zexuz <robin.edbom@gmail.com>
2020-09-24 18:15:59 +0200

    Setup Notification.* project

--- Padel.Notification.Runner/Startup.cs
+++ Padel.Notification.Runner/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Padel.Notification.Runner.Controllers;
@@ -9,6 +11,18 @@ namespace Padel.Notification.Runner
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new AutofacModule(_configuration));
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
---
+++ Padel.Notification/AutofacModule.cs
@@ -0,0 +1,20 @@
+using System;
+using Autofac;
+using Microsoft.Extensions.Configuration;
+
+namespace Padel.Notification
+{
+    public class AutofacModule : Module
+    {
+        private readonly IConfiguration _configuration;
+
+        public AutofacModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
