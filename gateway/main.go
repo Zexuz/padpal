b187707fe654039d3eafa059f71c801d2e9c490c
zexuz <robin.edbom@gmail.com>
2020-09-30 19:00:54 +0200

    Add notification docker file

--- gateway/main.go
+++ gateway/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"flag"
+	"github.com/mkdir-sweden/padpal/gateway/auth"
 	"github.com/mkdir-sweden/padpal/gateway/chat"
 	"github.com/mkdir-sweden/padpal/gateway/interceptors"
+	"github.com/mkdir-sweden/padpal/gateway/notification"
+	authpb "github.com/mkdir-sweden/padpal/gateway/protos/auth_v1"
 	"github.com/mkdir-sweden/padpal/gateway/protos/chat_v1"
+	noticitaionpb "github.com/mkdir-sweden/padpal/gateway/protos/notification_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	_ "google.golang.org/protobuf/types/descriptorpb"
@@ -33,7 +37,6 @@ func main() {
 	serverOps = append(serverOps, interceptors.WithJwtValidationUnaryInterceptor())
 
 	print("connecting...")
-
 	// This is good to have for HC maybe
 	opts = append(opts, grpc.WithBlock())
 	chatConn, err := grpc.Dial(getEnvOrDefault("CHAT_ADDRESS", "localhost:5001"), opts...)
@@ -43,17 +46,19 @@ func main() {
 	defer chatConn.Close()
 	print("connected to chat!")
 
-	//authConn, err := grpc.Dial("localhost:5002", opts...)
-	//if err != nil {
-	//	log.Fatalf("fail to dial: %v", err)
-	//}
-	//defer authConn.Close()
-	//
-	//notifiConn, err := grpc.Dial("localhost:5003", opts...)
-	//if err != nil {
-	//	log.Fatalf("fail to dial: %v", err)
-	//}
-	//defer notifiConn.Close()
+	authConn, err := grpc.Dial(getEnvOrDefault("IDENTITY_ADDRESS", "localhost:5002"), opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer authConn.Close()
+	print("connected to identity!")
+
+	notifiConn, err := grpc.Dial(getEnvOrDefault("NOTIFICATION_ADDRESS", "localhost:5003"), opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer notifiConn.Close()
+	print("connected to notification!")
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
@@ -63,8 +68,8 @@ func main() {
 
 	reflection.Register(s)
 	chatpb.RegisterChatServiceService(s, chat.NewChatService(chatConn))
-	//authpb.RegisterAuthServiceService(s, auth.NewAuthService(authConn))
-	//noticitaionpb.RegisterNotificationService(s, notification.NewNotificationService(authConn))
+	authpb.RegisterAuthServiceService(s, auth.NewAuthService(authConn))
+	noticitaionpb.RegisterNotificationService(s, notification.NewNotificationService(authConn))
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
