a1084be1ac3b94e530f77eb320a2d35a3c5b6416
Robin Edbom <Robin.edbom@gmail.com>
2020-09-30 19:02:57 +0200

    docker-chat (#9)
    * Update status bar
    
    * Add Dockerfile to gateway
    
    * Move chat to own folder
    
    * Docker
    
    * move identity
    
    * Add identity docker file
    
    * Add notification docker file
    
    * Updated gitignore

--- flutter_app/pad_pal/lib/main.dart
+++ flutter_app/pad_pal/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:pad_pal/app.dart';
 import 'package:pad_pal/app_push.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness:Brightness.dark,
+  ));
+
   WidgetsFlutterBinding.ensureInitialized();
 
   var env = "dev";
--- gateway/main.go
+++ gateway/main.go
@@ -6,19 +6,28 @@ import (
 	"github.com/mkdir-sweden/padpal/gateway/chat"
 	"github.com/mkdir-sweden/padpal/gateway/interceptors"
 	"github.com/mkdir-sweden/padpal/gateway/notification"
-	"github.com/mkdir-sweden/padpal/gateway/protos/auth_v1"
+	authpb "github.com/mkdir-sweden/padpal/gateway/protos/auth_v1"
 	"github.com/mkdir-sweden/padpal/gateway/protos/chat_v1"
-	"github.com/mkdir-sweden/padpal/gateway/protos/notification_v1"
+	noticitaionpb "github.com/mkdir-sweden/padpal/gateway/protos/notification_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	_ "google.golang.org/protobuf/types/descriptorpb"
 	"log"
 	"net"
+	"os"
 )
 
 const (
 	port = ":50051"
 )
+
+func getEnvOrDefault(name, def string) string {
+	value, found := os.LookupEnv(name)
+	if !found {
+		return def
+	}
+	return value
+}
 
 func main() {
 	flag.Parse()
@@ -27,25 +36,29 @@ func main() {
 	opts = append(opts, grpc.WithInsecure())
 	serverOps = append(serverOps, interceptors.WithJwtValidationUnaryInterceptor())
 
+	print("connecting...")
 	// This is good to have for HC maybe
 	opts = append(opts, grpc.WithBlock())
-	chatConn, err := grpc.Dial("localhost:5001", opts...)
+	chatConn, err := grpc.Dial(getEnvOrDefault("CHAT_ADDRESS", "localhost:5001"), opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer chatConn.Close()
+	print("connected to chat!")
 
-	authConn, err := grpc.Dial("localhost:5002", opts...)
+	authConn, err := grpc.Dial(getEnvOrDefault("IDENTITY_ADDRESS", "localhost:5002"), opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer authConn.Close()
+	print("connected to identity!")
 
-	notifiConn, err := grpc.Dial("localhost:5003", opts...)
+	notifiConn, err := grpc.Dial(getEnvOrDefault("NOTIFICATION_ADDRESS", "localhost:5003"), opts...)
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	defer notifiConn.Close()
+	print("connected to notification!")
 
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
