45068227b861d6619b6346dbf71b740324132e03
zexuz <robin.edbom@gmail.com>
2020-10-01 20:39:25 +0200

    Fix docker-compose.yml

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -f Dockerfile.chat . -t docker.pkg.github.com/mkdir-sweden/padpal/chat:latest
-docker build -f Dockerfile.identity . -t docker.pkg.github.com/mkdir-sweden/padpal/identity:latest
-docker build -f Dockerfile.notification . -t docker.pkg.github.com/mkdir-sweden/padpal/notification:latest
+docker build -f Dockerfile.chat . -t docker.pkg.github.com/mkdir-sweden/padpal/chat:latest && docker push docker.pkg.github.com/mkdir-sweden/padpal/chat:latest
+docker build -f Dockerfile.identity . -t docker.pkg.github.com/mkdir-sweden/padpal/identity:latest && docker push docker.pkg.github.com/mkdir-sweden/padpal/identity:latest
+docker build -f Dockerfile.notification . -t docker.pkg.github.com/mkdir-sweden/padpal/notification:latest && docker push docker.pkg.github.com/mkdir-sweden/padpal/notification:latest
