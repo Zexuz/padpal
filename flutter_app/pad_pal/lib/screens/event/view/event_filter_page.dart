043163a7019b9b2e45efe4fe07385e2476e94fec
zexuz <robin.edbom@gmail.com>
2020-10-10 23:21:05 +0200

    PAD-13 Add GoogleMaps intregration Add google maps/filter

--- flutter_app/pad_pal/lib/screens/event/view/event_filter_page.dart
+++ flutter_app/pad_pal/lib/screens/event/view/event_filter_page.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:pad_pal/components/app_bar/app_bar.dart';
+import 'package:pad_pal/theme.dart';
 
 class EventFilterPage extends StatelessWidget {
   static Route<void> route() {
@@ -28,38 +31,135 @@ class MapSample extends StatefulWidget {
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  Set<Circle> _circles = Set();
+
+  double _currentSliderValue = 10.0;
+  RangeValues _currentRangeValues = RangeValues(1, 3);
+
+  LatLng position;
+
+  static final CameraPosition _swedenCameraPosistion = CameraPosition(
+    target: LatLng(58.21, 14.53),
+    zoom: 3.0,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Future<void> _setCameraToMyLocation() async {
+    const myZoom = 14.0;
+    final position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    await _setCameraPosition(LatLng(position.latitude, position.longitude), myZoom);
+  }
+
+  Future<void> _setCameraPosition(LatLng latLng, double zoom) async {
+    final controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+      target: latLng,
+      zoom: zoom,
+    )));
+  }
+
+  Future<void> _updateMapPositionWithCurrentLocation() async {
+    // print("asdkjasd");
+    // final pos = await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    // setState(() {
+    //   print("robin $pos");
+    //   position = LatLng(pos.latitude, pos.longitude);
+    //   print(position);
+    // });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      appBar: CustomAppBar(
+        title: "Settings",
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24, 12, 24, 12),
+            child: TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Search by town/city, area or postcode',
+              ),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              zoomControlsEnabled: false,
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              circles: _circles,
+              initialCameraPosition: _swedenCameraPosistion,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+                _updateMapPositionWithCurrentLocation();
+                _setCameraToMyLocation();
+              },
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Distance"),
+                  Text("${_currentSliderValue.round()} km"),
+                ],
+              ),
+              Slider(
+                value: _currentSliderValue,
+                min: 1,
+                max: 100,
+                divisions: 100,
+                label: "${_currentSliderValue.round()} km",
+                onChanged: (double value) {
+                  setState(() {
+                    print(position);
+                    if(position != null){
+                      print(position);
+                      _circles.add(Circle(
+                        circleId: CircleId("1"),
+                        center: position,
+                        fillColor: AppTheme.secondaryColorOrange.withOpacity(0.12),
+                        strokeColor: AppTheme.secondaryColorOrange,
+                        strokeWidth: 1,
+                        radius: value,
+                      ));
+                    }
+                    _currentSliderValue = value;
+                  });
+                },
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Timespan"),
+                  Text("${_currentRangeValues.start.round()}-${_currentRangeValues.end.round()} days"),
+                ],
+              ),
+              RangeSlider(
+                values: _currentRangeValues,
+                min: 0,
+                max: 14,
+                divisions: 14,
+                labels: RangeLabels(
+                    _currentRangeValues.start.round().toString(), _currentRangeValues.end.round().toString()),
+                onChanged: (value) {
+                  setState(() {
+                    _currentRangeValues = value;
+                  });
+                },
+              )
+            ],
+          )
+        ],
       ),
     );
   }
-
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-  }
 }
-
