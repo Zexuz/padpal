1fb0fc42b626a922ea4fe0128e407c8543325804
Robin Edbom <Robin.edbom@gmail.com>
2020-10-10 23:22:00 +0200

    Pad 13 (#14)
    * Add correct lat long
    
    * PAD-13 Add GoogleMaps intregration Add filter page
    
    * PAD-13 Add GoogleMaps intregration Add google maps
    
    * PAD-13 Add GoogleMaps intregration Add google maps/filter

--- flutter_app/pad_pal/lib/screens/event/view/event_page.dart
+++ flutter_app/pad_pal/lib/screens/event/view/event_page.dart
@@ -4,6 +4,8 @@ import 'package:pad_pal/components/components.dart';
 import 'package:pad_pal/screens/create_event/create_event.dart';
 import 'package:pad_pal/theme.dart';
 
+import 'event_filter_page.dart';
+
 class EventPage extends StatelessWidget {
   const EventPage();
 
@@ -14,15 +16,22 @@ class EventPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title: 'Events nearby', actions: [
-        FlatButton(
-          onPressed: () => {Navigator.of(context).push<void>(CreateEventPage.route())},
-          child: Text(
-            'Create',
-            style: TextStyle(color: AppTheme.primary, fontWeight: FontWeight.w600),
-          ),
-        )
-      ]),
+      appBar: CustomAppBar(
+        title: 'Events nearby',
+        leading: IconButton(
+          icon: Icon(Icons.filter_alt, color: Colors.black),
+          onPressed: () => Navigator.push(context, EventFilterPage.route()),
+        ),
+        actions: [
+          FlatButton(
+            onPressed: () => {Navigator.of(context).push<void>(CreateEventPage.route())},
+            child: Text(
+              'Create',
+              style: TextStyle(color: AppTheme.primary, fontWeight: FontWeight.w600),
+            ),
+          )
+        ],
+      ),
       backgroundColor: const Color(0xFFDDE2E9),
       body: Container(
         padding: const EdgeInsets.all(12.0),
