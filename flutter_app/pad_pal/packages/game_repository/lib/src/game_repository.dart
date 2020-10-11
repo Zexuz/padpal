00d2b66fdc22ec1d92dbe79597b21de08035ebb2
Robin Edbom <Robin.edbom@gmail.com>
2020-10-12 01:14:58 +0200

    Pad 24 (#17)
    * PAD-24 Use filter cubit Create game_repository.dart, remove user_repository.dart
    
    * PAD-24 Use filter cubit get games from backend with filter
    
    * PAD-24 Use filter cubit some filter fixes

---
+++ flutter_app/pad_pal/packages/game_repository/lib/src/game_repository.dart
@@ -0,0 +1,24 @@
+import 'package:grpc/grpc.dart';
+import 'package:grpc_helpers/grpc_helpers.dart';
+import 'package:game_repository/generated/game_v1/game_service.pbgrpc.dart';
+
+class GameRepository {
+  GameRepository({GameClient gameClient, TokenManager tokenManager})
+      : _gameClient = gameClient ?? GameClient(GrpcChannelFactory().createChannel()),
+        _tokenManager = tokenManager ?? TokenManager();
+
+  final GameClient _gameClient;
+  final TokenManager _tokenManager;
+
+  Future<List<PublicGameInfo>> findGames(GameFilter filter) async {
+    final callOptions =
+        CallOptions(metadata: {'Authorization': "Bearer ${(await _tokenManager.getAccessToken()).token}"});
+
+    final request = FindGamesRequest()..filter = filter;
+
+    final call = _gameClient.findGames(request, options: callOptions);
+
+    final protoRes = await call;
+    return protoRes.games;
+  }
+}
